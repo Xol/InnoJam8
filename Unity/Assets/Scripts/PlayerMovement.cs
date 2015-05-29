b28114c0919fc8c1cf7afd32f5c0e0ac161d7c33
lars.engel <lars.engel@innogames.com>
2015-05-30 00:52:18 +0200

    Prefabs and so on

--- Unity/Assets/Scripts/PlayerMovement.cs
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,21 @@ public class PlayerMovement : MonoBehaviour {
 	public Vector3 speed;
 	public Vector3 invSpeed;
 	private Rigidbody rb;
+	private BoxCollider bc;
+	private Vector3 boxColliderSize;
 
 	// Use this for initialization
 	void Start () {
 		speed = new Vector3 (10, 0, 0);
 		invSpeed = -1 * speed;
 		rb = this.gameObject.GetComponent<Rigidbody> ();
+		bc = this.gameObject.GetComponent<BoxCollider> ();
+		boxColliderSize = bc.size;
+		//rb.centerOfMass = new Vector3 (-1.3f, -1.3f, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (rb.velocity);
 		if (rb.velocity.x <= 25f && rb.velocity.x >= -25f) {
 			if (Input.GetKey (KeyCode.LeftArrow)) {
 				rb.AddForce (invSpeed);	
@@ -35,6 +39,20 @@ public class PlayerMovement : MonoBehaviour {
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			rb.useGravity = true;
 
+			if (rb.velocity.y >= (float)(-0.1) && rb.velocity.y <= (float)(0.1)) {
+				bc.size = new Vector3 (boxColliderSize.x, boxColliderSize.y / 2, bc.size.z);
+				bc.center = new Vector3(0,-1,0);
+			} else {
+				bc.size = boxColliderSize;
+				bc.center = Vector3.zero;
+			}
+		} else {
+			bc.size = boxColliderSize;
+			bc.center = Vector3.zero;
+		}
+
+		if (this.transform.position.y <= 4f) {
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.identity, 250 * Time.deltaTime );		
 		}
 	}
 }
