0ab205dc46cdb80e3cefdb8549265d5b8970a015
lars.engel <lars.engel@innogames.com>
2015-05-31 00:53:43 +0200

    Spring animation

--- Unity/Assets/Scripts/SpringAnimation.cs
+++ Unity/Assets/Scripts/SpringAnimation.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class SpringAnimation : MonoBehaviour {
 
+	public Sprite[] idle;
+	private int index;
+	private float FPS;
+
 	// Use this for initialization
 	void Start () {
-	
+		idle = Resources.LoadAll<Sprite>("idle");
+		FPS = 20;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (idle.Length == 0)
+			return;
+		float index = Time.time * FPS;
+		index = index % idle.Length;
+		gameObject.GetComponent<SpriteRenderer>().sprite = idle [(int)index];
 	}
 }
