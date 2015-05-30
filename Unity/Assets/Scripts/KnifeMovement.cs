88afbb5c48176f349cee194c4b2c49926ab417ea
lars.engel <lars.engel@innogames.com>
2015-05-30 13:08:11 +0200

    Knife Throwing

--- Unity/Assets/Scripts/KnifeMovement.cs
+++ Unity/Assets/Scripts/KnifeMovement.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class KnifeMovement : MonoBehaviour {
 
+	private GameObject Player;
+	private float speed;
+	private Vector3 velVec;
+
+
 	// Use this for initialization
 	void Start () {
-	
+		Player = GameObject.Find ("Player");
+		speed = 50;
+		velVec = new Vector3 ();
+		velVec.x = Mathf.Cos(Player.transform.eulerAngles.z * Mathf.Deg2Rad) * speed;
+		velVec.y = Mathf.Sin(Player.transform.eulerAngles.z * Mathf.Deg2Rad) * speed;
+		velVec.z = 0;
+		this.transform.rotation = Player.transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		this.gameObject.GetComponent<Rigidbody> ().velocity = velVec;
 
 	}
 }
--- Unity/Assets/Scripts/KnifeShoot.cs
+++ Unity/Assets/Scripts/KnifeShoot.cs
@@ -3,17 +3,19 @@ using System.Collections;
 
 public class KnifeShoot : MonoBehaviour {
 
+	public GameObject knife;
+	public GameObject spawnPoint;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKey (KeyCode.Space)) {
-
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Instantiate(knife, spawnPoint.gameObject.transform.position, Quaternion.identity);
 		}
 	}
 }
