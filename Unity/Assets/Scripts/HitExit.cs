230dfb12df2f25bc0ce6383210d1ef58fe62d70c
Xol <andre@andrerother.de>
2015-05-31 13:19:56 +0200

    Enter and Exit Portal included. Exit Sign and new Textures added.

---
+++ Unity/Assets/Scripts/HitExit.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitExit : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("Exit wurde getroffen");
+		//Restart level
+	}
+}
